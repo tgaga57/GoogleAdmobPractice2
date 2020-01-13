9ac8c63349c034d61e461932aca5b81dd6363645
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2020-01-13 13:06:02 +0900

    バナー広告の設定

---
+++ Pods/Target Support Files/Pods-googleAdmobPractice/Pods-googleAdmobPractice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_googleAdmobPractice : NSObject
+@end
+@implementation PodsDummy_Pods_googleAdmobPractice
+@end
---
+++ googleAdmobPractice/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  googleAdmobPractice
+//
+//  Created by 志賀大河 on 2020/01/13.
+//  Copyright © 2020 Taigashiga. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class ViewController: UIViewController {
+
+    
+    // bannerView
+    @IBOutlet weak var bannerView: GADBannerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 広告出すのに必要なもの
+        bannerView.adUnitID = ""
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+    }
+
+
+}
+
